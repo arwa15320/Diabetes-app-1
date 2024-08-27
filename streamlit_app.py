90baa2af0feef286b9d80a4ca524a7bb906e011b
arwa15320 <97008135+arwa15320@users.noreply.github.com>
2024-08-28 00:59:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
 
-# Load the trained model
-model = joblib.load('/mnt/data/diabetes_model (1).pkl')
+
 
 # Streamlit app title
 st.title("Diabetes Prediction")
