286d586e4abf3ebb70308ecbcf2b82ecc2763e46
arwa15320 <97008135+arwa15320@users.noreply.github.com>
2024-08-28 01:17:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ st.title("Diabetes Prediction")
 st.subheader("Please enter the features below:")
 
 # Collecting input features based on the typical diabetes dataset
+age = st.number_input("Age", min_value=0, max_value=120, value=33)
 pregnancies = st.number_input("Pregnancies", min_value=0, max_value=20, value=0)
 glucose = st.number_input("Glucose", min_value=0, max_value=200, value=120)
 insulin = st.number_input("Insulin", min_value=0, max_value=846, value=79)
 bmi = st.number_input("BMI", min_value=0.0, max_value=67.0, value=25.0)
-dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.5, value=0.5)
-age = st.number_input("Age", min_value=0, max_value=120, value=33)
+diabetes_pedigree= st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.5, value=0.5)
 
 # Button to trigger the prediction
 if st.button("Predict"):
@@ -27,12 +27,13 @@ if st.button("Predict"):
         
         # Create a DataFrame for the input features
         input_data = pd.DataFrame({
+            'Age': [age],
             'Pregnancies': [pregnancies],
             'Glucose': [glucose],
             'Insulin': [insulin],
             'BMI': [bmi],
-            'DiabetesPedigreeFunction': [dpf],
-            'Age': [age]
+            'DiabetesPedigreeFunction': [dpf]
+            
         })
 
         # Perform prediction
